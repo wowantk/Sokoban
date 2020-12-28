1c17ea97c935fa514ae012d2b22d0b5b35a4e792
Pavel12345 <beskor1993@icloud.com>
2020-12-28 19:53:31 +0200

    Initial Commit

---
+++ Blocks 1.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Blocks 1.3
+//
+//  Created by Павел Бескоровайный on 28.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
